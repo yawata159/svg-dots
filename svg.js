5083e633d8000fbbeb42273ffa67b2bcde992689
yawata159 <seijiyawata@gmail.com>
2017-02-28 19:49:33 -0500

    get rid of lines bool

--- svg.js
+++ svg.js
@@ -1,33 +1,33 @@
-var s = document.querySelector("#svg");
+function main() {
+    var s = document.querySelector("#svg");
+    var namespace = "http://www.w3.org/2000/svg";
+    var prevX, prevY;
 
-var namespace = "http://www.w3.org/2000/svg";
+    function clickHandler(e) {
+        var x = e.offsetX;
+        var y = e.offsetY;
+        
+        var line = document.createElementNS(namespace, "line");
+        line.setAttribute("x1", prevX ? prevX: x);
+        line.setAttribute("y1", prevY ? prevY: y);
+        line.setAttribute("x2", x);
+        line.setAttribute("y2", y);
+        line.setAttribute("style", "stroke:dodgerblue; stroke-width:2");
 
-var prevX, prevY;
-var lines = false;
+        var circle = document.createElementNS(namespace, "circle");
+        circle.setAttribute("cx", x);
+        circle.setAttribute("cy", y);
+        circle.setAttribute("r", "20");
+        circle.setAttribute("fill", "firebrick");
 
-function clickHandler(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-    
-    var line = document.createElementNS(namespace, "line");
-    line.setAttribute("x1", lines ? prevX: x);
-    line.setAttribute("y1", lines ? prevY: y);
-    line.setAttribute("x2", x);
-    line.setAttribute("y2", y);
-    line.setAttribute("style", "stroke:dodgerblue; stroke-width:2");
+        s.appendChild(circle);
+        s.appendChild(line);
 
-    var circle = document.createElementNS(namespace, "circle");
-    circle.setAttribute("cx", x);
-    circle.setAttribute("cy", y);
-    circle.setAttribute("r", "20");
-    circle.setAttribute("fill", "salmon");
+        prevX = x;
+        prevY = y;
+    }
 
-    s.appendChild(circle);
-    s.appendChild(line);
-
-    lines = true;
-    prevX = x;
-    prevY = y;
+    s.addEventListener("click", clickHandler);
 }
 
-s.addEventListener("click", clickHandler);
+window.onload = main;
